0f818dc13efbd5c1d352e35a7d7ed5b58e761664
Jason-Li <liyike1988@gmail.com>
2025-02-13 14:10:51 -0800

    main update

--- main.py
+++ main.py
@@ -1,27 +1,53 @@
 import requests
 import streamlit as st
-from dotenv import load_dotenv
-import os
+import json
 
-load_dotenv()
-
-BASE_API_URL = "https://api.langflow.astra.datastax.com"
-LANGFLOW_ID = ""
-FLOW_ID = ""
-APPLICATION_TOKEN = os.environ.get("APP_TOKEN")
+BASE_API_URL = "http://127.0.0.1:7860"
+FLOW_ID = "b009cd29-dc18-4745-bf8c-8e7aec87c5cb"
 ENDPOINT = "customer" # The endpoint name of the flow
 
+# You can tweak the flow by adding a tweaks dictionary
+# e.g {"OpenAI-XXXXX": {"model_name": "gpt-4"}}
+# TWEAKS = {
+#   "ChatInput-tMEFZ": {},
+#   "ChatOutput-6CeuH": {},
+#   "Agent-exkbp": {},
+#   "Prompt-URE69": {},
+#   "AstraDB-Asl7b": {},
+#   "ParseData-llIwd": {},
+#   "File-N7e0b": {},
+#   "SplitText-Obey4": {},
+#   "AstraDB-Pqrra": {},
+#   "Agent-XTEYC": {},
+#   "Agent-qJEec": {},
+#   "AstraDBToolComponent-B5jhp": {},
+#   "AstraDBToolComponent-NK9Xm": {}
+# }
 
-def run_flow(message: str) -> dict:
-    api_url = f"{BASE_API_URL}/lf/{LANGFLOW_ID}/api/v1/run/{ENDPOINT}"
+def run_flow(message: str,
+  output_type: str = "chat",
+  input_type: str = "chat") -> dict:
+    """
+    Run a flow with a given message and optional tweaks.
+
+    :param message: The message to send to the flow
+    :param endpoint: The ID or the endpoint name of the flow
+    :param tweaks: Optional tweaks to customize the flow
+    :return: The JSON response from the flow
+    """
+    api_url = f"{BASE_API_URL}/api/v1/run/{ENDPOINT}"
 
     payload = {
         "input_value": message,
-        "output_type": "chat",
-        "input_type": "chat",
+        "output_type": output_type,
+        "input_type": input_type,
     }
+    headers = None
+    # if tweaks:
+    #     payload["tweaks"] = tweaks
+    # if api_key:
+    #     headers = {"x-api-key": api_key}
 
-    headers = {"Authorization": "Bearer " + APPLICATION_TOKEN, "Content-Type": "application/json"}
     response = requests.post(api_url, json=payload, headers=headers)
     return response.json()
 
@@ -43,6 +69,9 @@ def main():
             st.markdown(response)
         except Exception as e:
             st.error(str(e))
+
+# res = run_flow("what are the shipment times?")
+# print(res)
 
 if __name__ == "__main__":
     main()
